1e7ba8d37b9e47b4d931d4d611a35f080eb3c93e
ohurtado <oscar.hurtado@axity.com>
2024-02-20 11:16:11 -0600

    se modifica conteo.py

--- conteo.py
+++ conteo.py
@@ -1,7 +1,7 @@
-# Este script muestra un conteo del 1 al 200
+
 
 # Bucle para recorrer los números del 1 al 200
-for numero in range(1, 201):
+for numero in range(0, 200):
   # Imprimir el número actual
   print(numero)
 
