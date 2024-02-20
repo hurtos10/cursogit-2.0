8cc158c17930b8fd2ab738a775e3acf58c733f33
ohurtado <oscar.hurtado@axity.com>
2024-02-20 11:22:31 -0600

    se cambio linea de print

--- suma de 2 numeros.py
+++ suma de 2 numeros.py
@@ -6,12 +6,12 @@ firstnumber = int(input("Introduzca el primer número: "))
 print("EL NUMERO INTRODUCIDO ES --->:", firstnumber)
 
 # Pedir al usuario que introduzca el segundo número
-secondnumber = int(input("Introduzca el segundo número: "))
+secondnumberz = int(input("Introduzca el segundo número: "))
 
-print("EL NUMERO INTRODUCIDO ES --->:", secondnumber)
+print("EL NUMERO INTRODUCIDO ES --->:", secondnumberz)
 
 # Calcular la suma de los dos números
-result = firstnumber + secondnumber
+result = firstnumber + secondnumberz
 
 # Mostrar el resultado de la suma
 print("LA SUMATORIA RESULTANTE ES --->:", result)
