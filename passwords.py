c71bbfd2ff6cfafcc482d7a1d8130373326e9a77
Yonatan Ortiz <yonatan.ortiz@axity.com>
2024-02-20 16:44:14 -0600

    se agrego un nuevo script en python

---
+++ passwords.py
@@ -0,0 +1,19 @@
+'''
+GENERADOR DE CONTRASEÑAS SEGURAS
+'''
+
+import secrets
+import string
+
+def generar_contrasena(longitud=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(secrets.choice(caracteres) for _ in range(longitud))
+    return contrasena
+
+# Solicitar al usuario la longitud deseada para la contraseña
+longitud_contraseña = int(input("Ingrese la longitud deseada para la contraseña: "))
+
+# Generar la contraseña y mostrarla
+contraseña_generada = generar_contrasena(longitud_contraseña)
+print(f"Contraseña generada: {contraseña_generada}")
+
