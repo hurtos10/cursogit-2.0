bd0bef2c6ec4985bc0508d2dfac10d2af9998f76
Yonatan Ortiz <yonatan.ortiz@axity.com>
2024-02-20 16:52:58 -0600

    se agrego una validacion al script passwords

--- passwords.py
+++ passwords.py
@@ -6,12 +6,15 @@ import secrets
 import string
 
 def generar_contrasena(longitud=12):
-    caracteres = string.ascii_letters + string.digits + string.punctuation
+    caracteres = string.ascii_letters  + string.digits + string.punctuation
     contrasena = ''.join(secrets.choice(caracteres) for _ in range(longitud))
     return contrasena
 
 # Solicitar al usuario la longitud deseada para la contraseña
 longitud_contraseña = int(input("Ingrese la longitud deseada para la contraseña: "))
+if longitud_contraseña < 8:
+    print('La longitud de la contraseña es muy chica.')
+    exit()
 
 # Generar la contraseña y mostrarla
 contraseña_generada = generar_contrasena(longitud_contraseña)
